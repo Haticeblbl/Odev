5861abb22a1c1c687f0dc4a09a4f66ec388ed676
Haticeblbl <105600377+Haticeblbl@users.noreply.github.com>
2022-05-15 19:42:05 +0300

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace WindowsFormsApp12
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OyunEkranıPanel = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TutucuPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbsure = new System.Windows.Forms.Label();
+            this.OyunEkranıPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OyunEkranıPanel
+            // 
+            this.OyunEkranıPanel.BackColor = System.Drawing.Color.Salmon;
+            this.OyunEkranıPanel.Controls.Add(this.TutucuPanel);
+            this.OyunEkranıPanel.Controls.Add(this.panel2);
+            this.OyunEkranıPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OyunEkranıPanel.Location = new System.Drawing.Point(0, 0);
+            this.OyunEkranıPanel.Name = "OyunEkranıPanel";
+            this.OyunEkranıPanel.Size = new System.Drawing.Size(800, 450);
+            this.OyunEkranıPanel.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MistyRose;
+            this.panel2.Controls.Add(this.lbsure);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(573, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(227, 450);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // TutucuPanel
+            // 
+            this.TutucuPanel.BackColor = System.Drawing.Color.DarkSalmon;
+            this.TutucuPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TutucuPanel.Location = new System.Drawing.Point(0, 351);
+            this.TutucuPanel.Name = "TutucuPanel";
+            this.TutucuPanel.Size = new System.Drawing.Size(573, 99);
+            this.TutucuPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kalan Süre(sn)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yapılan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kalan:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Sienna;
+            this.label4.Location = new System.Drawing.Point(57, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 4;
+            // 
+            // lbsure
+            // 
+            this.lbsure.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbsure.BackColor = System.Drawing.SystemColors.ControlText;
+            this.lbsure.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbsure.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbsure.Location = new System.Drawing.Point(13, 57);
+            this.lbsure.Name = "lbsure";
+            this.lbsure.Size = new System.Drawing.Size(202, 64);
+            this.lbsure.TabIndex = 5;
+            this.lbsure.Text = "00";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OyunEkranıPanel);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
+            this.OyunEkranıPanel.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel OyunEkranıPanel;
+        private System.Windows.Forms.Panel TutucuPanel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbsure;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1.Contrete;
+using ClassLibrary1.Enum;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form2 : Form
+    {
+        private readonly Oyun _oyun;
+
+
+
+        public Form2()
+        {
+            InitializeComponent();
+            _oyun = new Oyun(TutucuPanel, OyunEkranıPanel);
+            _oyun.GecenSureDegisti += Oyun_GecenSureDegisti;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Enter:
+                    _oyun.Baslat();
+                    break;
+                case Keys.Right:
+                    _oyun.HareketEttir(Yon.sağa);
+                    break;
+                case Keys.Left:
+                    _oyun.HareketEttir(Yon.sola);
+                    break;
+                case Keys.Down:
+                    _oyun.HareketEttir(Yon.aşağı);
+                    break;
+                case Keys.Space:
+                    _oyun.Topla();
+                    break;
+                case Keys.P:
+                    _oyun.Durdur();
+                    break; 
+            }
+        }
+        private void Oyun_GecenSureDegisti(object sender, EventArgs e)
+        {
+            lbsure.Text = $"{_oyun.GecenSure.Minutes}:{_oyun.GecenSure.Seconds.ToString("D2")}";
+        }
+    }
+}
